896d1cb537385cd18abe7a919c991799d37e3f32
Juribiyan <snivy@ponyasha.ru>
2017-03-20 11:16:16 +0600

    Update scroll-gestures.user.js

--- scroll-gestures.user.js
+++ scroll-gestures.user.js
@@ -1,40 +1,34 @@
 // ==UserScript==
 // @name         Scroll Gestures
 // @namespace    http://juribiyan.github.com
-// @version      1.0
+// @version      2.0
 // @description  Scroll to top or bottom using RMB+scroll
 // @author       Snivy
 // @match         *://*/*
 // @grant        none
 // ==/UserScript==
 
-var rmbd;
+var rmbd
 
 document.body.onmousedown = function (e) {
-    var isRightMB;
-    e = e || window.event;
-
-    if ("which" in e)
-        isRightMB = e.which == 3; 
-    else if ("button" in e)
-        isRightMB = e.button == 2; 
-
-    if(isRightMB) rmbd = true;
+  if(e.button == 2) 
+    rmbd = true
 } 
 
 document.body.oncontextmenu = function (e) {
-    if(rmbd === 1) {
-    	e.preventDefault();
-	}
-    rmbd = false;
+  if(rmbd === 1) {
+    e.preventDefault();
+  }
+  rmbd = false
 } 
 
 window.onmousewheel = function(ev) {
-	if(rmbd) {
-		if(ev.wheelDelta > 0) 
-			document.body.scrollTop = document.documentElement.scrollTop = 0;
-		if(ev.wheelDelta < 0) 
-			window.scrollTo(0, document.body.scrollHeight);
-		rmbd = 1;
-	}
+  if(rmbd) {
+    ev.preventDefault()
+    if(ev.wheelDelta > 0) 
+      scroll(0, 0)
+    else if(ev.wheelDelta < 0) 
+      scroll(0, document.body.scrollHeight)
+    rmbd = 1
+  }
 }
