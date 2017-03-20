8f5ecbc0e1ecee49b4c10bfa4062d5c762a1d08b
Juribiyan <snivy@ponyasha.ru>
2017-03-20 15:24:28 +0600

    Saving intermediate state

--- scroll-gestures.user.js
+++ scroll-gestures.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         Scroll Gestures
 // @namespace    http://juribiyan.github.com
-// @version      2.0
+// @version      3.0
 // @description  Scroll to top or bottom using RMB+scroll
 // @author       Snivy
 // @match         *://*/*
 // @grant        none
 // ==/UserScript==
 
-var rmbd
+var rmbd, returnTo = false
 
 document.body.onmousedown = function (e) {
   if(e.button == 2) 
@@ -17,7 +17,7 @@ document.body.onmousedown = function (e) {
 
 document.body.oncontextmenu = function (e) {
   if(rmbd === 1) {
-    e.preventDefault();
+    e.preventDefault()
   }
   rmbd = false
 } 
@@ -25,10 +25,30 @@ document.body.oncontextmenu = function (e) {
 window.onmousewheel = function(ev) {
   if(rmbd) {
     ev.preventDefault()
-    if(ev.wheelDelta > 0) 
-      scroll(0, 0)
-    else if(ev.wheelDelta < 0) 
-      scroll(0, document.body.scrollHeight)
+    var currentPosition = document.documentElement.scrollTop
+    , atTop = currentPosition === 0
+    , atBottom = currentPosition === (document.documentElement.scrollHeight - document.documentElement.clientHeight)
+    , atEitherEnd = atTop || atBottom
+    if(ev.wheelDelta > 0) {
+      if (atBottom && returnTo) {
+        scroll(0, returnTo)
+      }
+      else {
+        if (!atEitherEnd)
+          returnTo = currentPosition
+        scroll(0, 0)
+      }
+    }
+    else if(ev.wheelDelta < 0) {
+      if (atTop && returnTo) {
+        scroll(0, returnTo)
+      }
+      else {
+        if (!atEitherEnd)
+          returnTo = currentPosition
+        scroll(0, document.body.scrollHeight)
+      }
+    }
     rmbd = 1
   }
 }
